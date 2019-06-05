23bd6a450b7ae90bba750711ddfb1e20229b9e35
Marlon Leoner <marlon.leoner@gmail.com>
2019-06-05 00:09:08 -0300

    RMI_PORT and RMI_HOST removed

--- src/UserChat.java
+++ src/UserChat.java
@@ -3,11 +3,6 @@ import java.rmi.server.UnicastRemoteObject;
 
 public class UserChat extends UnicastRemoteObject implements IUserChat {
 
-   // PORT
-   private static final int RMI_PORT = 2020;
-   // URL
-   private static final String RMI_HOST = "rmi://localhost/";
-
    private UserGUI userGUI;
 
    public UserChat(UserGUI userGUI) throws RemoteException {
